0e5327d810ac51566203336157c38d9e3cc9038f
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2022-11-13 21:03:32 +0100

    date and time formatting function added

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -4,13 +4,13 @@ import { useCurrentDate } from "../utils/useCurrentDate";
 const Clock = () => {
     const date = useCurrentDate();
 
-    const formatDate = date.toLocaleDateString("pl", {
+    const setFormatDate = (date) => date.toLocaleDateString("pl", {
         weekday: "long",
         day: "numeric",
         month: "long",
     });
 
-    const formatTime = date.toLocaleTimeString("pl", {
+    const setFormatTime = (date) => date.toLocaleTimeString("pl", {
         hour: "numeric",
         minute: "numeric",
         second: "numeric",
@@ -18,7 +18,7 @@ const Clock = () => {
 
     return (
         < p className="clock" >
-            Dzisiaj jest {formatDate}, {formatTime}
+            Dzisiaj jest {setFormatDate(date)}, {setFormatTime(date)}
         </p >
     )
 };
