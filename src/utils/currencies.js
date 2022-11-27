64a4bb3bfa882798f30c0ee2b3bd8ed374ae2d87
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2022-11-27 21:33:44 +0100

    utils folder removed

--- src/utils/currencies.js
+++
@@ -1,5 +0,0 @@
-export const currencies = [
-    { short: "EUR", name: "Euro", rate: 4.86 },
-    { short: "USD", name: "Dolar amerykański", rate: 4.95 },
-    { short: "GBP", name: "Funt brytyjski", rate: 5.52 },
-];
